9978537f7379ff62a8817f0d2819c7de4bc56c68
Kasper Zutterman <kasper.zutterman@ugent.be>
2019-11-08 17:16:24 +0100

    Added insertionSort import

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import Array from './scripts/model/Array.js'
 import ArrayView from './scripts/view/ArrayView.js'
 import SelectionSort from './scripts/model/algorithms/SelectionSort.js'
+import InsertionSort from './scripts/model/algorithms/InsertionSort.js'
 
 let width = 1800;
 let height = 600;
@@ -9,7 +10,7 @@ let sortingCanvas = document.getElementById("sortingCanvas");
 let context = sortingCanvas.getContext("2d");
 context.translate(0, sortingCanvas.height);
 context.scale(1, -1);
-let array = new Array(300);
+let array = new Array(30);
 console.log();
 
 
@@ -17,7 +18,8 @@ let arrayView = new ArrayView(array, sortingCanvas, width, height);
 
 arrayView.drawArray();
 
-let sortAlgorithm = new SelectionSort(array);
+//let sortAlgorithm = new SelectionSort(array);
+let sortAlgorithm = new InsertionSort(array);
 
 let btnStep = document.getElementById("btnStep");
 btnStep.addEventListener("click", (e) => {
